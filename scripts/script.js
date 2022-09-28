7350540da4b889cd93359cbdb05f5326968dbb0b
Eugene Korobkin <90062386+EugeneCod@users.noreply.github.com>
2022-09-28 18:29:50 +0300

    refactor: remove comments

--- scripts/script.js
+++ scripts/script.js
@@ -2,14 +2,12 @@
 
 const buttons_play = document.querySelectorAll('.video__icon-play');
 const iframes_wrapper = document.querySelectorAll('.video__iframe-wrapper');
-// const iframes = document.querySelectorAll('.video__iframe');
 const covers = document.querySelectorAll('.video__cover');
 
 for (let i = 0; i < buttons_play.length; i++) {
   buttons_play[i].addEventListener ('click', function () {
     covers[i].classList.toggle('_active');
     iframes_wrapper[i].classList.toggle('_active');
-    // iframes[i].classList.toggle('_active');
     buttons_play[i].classList.toggle('_active');
   });
 }
